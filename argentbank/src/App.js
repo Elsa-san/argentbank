5d6aeff683b4da0d3a5aa829b5b57d709252c80a
Elsa-san <elsa.tartary@gmail.com>
2023-10-10 11:11:10 +0200

    routes

---
+++ argentbank/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+// Importing routing functions from React Router
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+    {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+
+])
+
+function App() {
+  return (
+    <>
+    <RouterProvider router={router}/>
+    </>
+  );
+}
+
+export default App;
+
